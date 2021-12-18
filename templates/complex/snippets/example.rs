d65112e2e645aaa25080e5e4318006a95abee1b2
RubixDev <silas.groh@t-online.de>
2021-12-18 17:53:33 +0100

    Add english versions of templates

---
+++ templates/complex/snippets/example.rs
@@ -0,0 +1,11 @@
+/// The main function
+pub fn main() -> Result<char> {
+    let name: &str = "World";
+    // Prints 'Hello, World!' to stdout
+    println!("Hello, {}!", name);
+    /*
+      Return the char 'x'
+      because this is a bad example
+    */
+    'x'
+}
